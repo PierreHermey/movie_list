ec18df4bed430f4dbecc62a1ad09a252d8645d1f
DuronStacy <stacy.d@codeur.online>
2018-05-24 16:51:51 +0200

    urlRewriting

--- model.php
+++ model.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $bdd = new PDO('mysql:host=localhost;dbname=movies;charset=utf8', 'root', 'stagiaire');
+    $bdd = new PDO('mysql:host=localhost;dbname=DB_Movie2;charset=utf8', 'root', 'root');
   }
   catch (Exception $e){
     die('Erreur : ' . $e->getMessage());
